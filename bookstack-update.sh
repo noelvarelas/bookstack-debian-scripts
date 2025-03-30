0726fab9cd689241a68042070a57480c098ed2e8
Noel Varelas <noel@noelvarelas.com>
2025-03-30 15:33:31 -0700

    fix hardcoded bookstack dir

--- bookstack-update.sh
+++ bookstack-update.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+BOOKSTACK_DIR="/var/www/bookstack"
+SCRIPT_USER="${SUDO_USER:-$USER}"
+
 # Warn user about auto-updating
 echo; echo "Please read the official release notes before updating. Some updates may require additional action."
 echo "https://www.bookstackapp.com/docs/admin/updates/#version-specific-instructions"
@@ -7,27 +10,17 @@ echo "Do not run this script without having a recent backup available."
 echo; [[ "$(read -e -p 'Are you sure you want to continue? [y/N] '; echo "$REPLY")" == [Yy]* ]] && echo || exit
 
 # Check we're running as root and exit if not
-if [[ $EUID -ne 0 ]]; then
-  echo "This script must be ran with root/sudo privileges." >&2
-  exit 1
-fi
+[[ $EUID -ne 0 ]] && echo "This script must be ran with root/sudo privileges." >&2 && exit 1
 
 # Check that the bookstack directory exists
-if [ ! -d "/var/www/bookstack" ]; then
-  echo "BookStack is not installed on this system." >&2
-  exit 1
-fi
+[ ! -d "$BOOKSTACK_DIR" ] && echo "BookStack is not installed on this system." >&2 && exit 1
 
 # Check that the bookstack owner is running the script
-dir_owner_uid=$(stat -c '%U' "/var/www/bookstack")
-SCRIPT_USER="${SUDO_USER:-$USER}"
-if [[ "$SCRIPT_USER" != "$dir_owner_uid" ]]; then
-  echo "This script must be run by the owner of /var/www/bookstack." >&2
-  exit 1
-fi
+dir_owner_uid=$(stat -c '%U' "$BOOKSTACK_DIR")
+[[ "$SCRIPT_USER" != "$dir_owner_uid" ]] && echo "This script must be run by the owner of $BOOKSTACK_DIR." >&2 && exit 1
 
 # Update commands
-cd /var/www/bookstack
+cd "$BOOKSTACK_DIR"
 git pull origin release
 sudo -u "$SCRIPT_USER" composer install --no-dev
 php artisan migrate
@@ -37,9 +30,9 @@ php artisan view:clear
 
 # Reset filesystem permissions
 echo "Resetting filesystem permissions..."; echo
-chown -R $SCRIPT_USER:www-data /var/www/bookstack
-chmod -R 755 /var/www/bookstack
-chmod -R 775 /var/www/bookstack/storage /var/www/bookstack/bootstrap/cache /var/www/bookstack/public/uploads
-chmod 640 /var/www/bookstack/.env
+chown -R "$SCRIPT_USER":www-data "$BOOKSTACK_DIR"
+chmod -R 755 "$BOOKSTACK_DIR"
+chmod -R 775 "$BOOKSTACK_DIR"/storage "$BOOKSTACK_DIR"/bootstrap/cache "$BOOKSTACK_DIR"/public/uploads
+chmod 640 "$BOOKSTACK_DIR"/.env
 
 echo "Done!"
